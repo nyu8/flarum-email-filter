a100888d81449e53d701170c0be98f3fac8b5337
Dizy <dev@dizy.cc>
2022-04-15 17:42:28 -0400

    Fix: make cases insensitive for blacklist

--- src/EmailCustomValidator.php
+++ src/EmailCustomValidator.php
@@ -9,98 +9,100 @@ use NYU8\FlarumEmailFilter\Rule;
 
 class EmailCustomValidator
 {
-  /**
-   * @var SettingsRepositoryInterface
-   */
-  protected $settings;
+    /**
+     * @var SettingsRepositoryInterface
+     */
+    protected $settings;
 
-  /**
-   * @var Translator
-   */
-  protected $translator;
+    /**
+     * @var Translator
+     */
+    protected $translator;
 
-  public function __construct(SettingsRepositoryInterface $settings, Translator $translator)
-  {
-    $this->settings = $settings;
-    $this->translator = $translator;
-  }
-
-  public function validate(string $email)
-  {
-    $activeRules = Rule::all()->where('active', 1);
-    $blacklistLiteralRules = $activeRules->where('rule_type', 3);
-    $blacklisted = false;
-
-    if (static::checkLiteral($email, $blacklistLiteralRules)) {
-      $this->raiseBlacklistException();
+    public function __construct(SettingsRepositoryInterface $settings, Translator $translator)
+    {
+        $this->settings = $settings;
+        $this->translator = $translator;
     }
 
-    $blackListRegexRules = $activeRules->where('rule_type', 4);
+    public function validate(string $email)
+    {
+        $activeRules = Rule::all()->where('active', 1);
+        $blacklistLiteralRules = $activeRules->where('rule_type', 3);
+        $blacklisted = false;
 
-    if (static::checkRegex($email, $blackListRegexRules)) {
-      $blacklisted = true;
+        if (static::checkLiteral($email, $blacklistLiteralRules, false)) {
+            $this->raiseBlacklistException();
+        }
+
+        $blackListRegexRules = $activeRules->where('rule_type', 4);
+
+        if (static::checkRegex($email, $blackListRegexRules, false)) {
+            $blacklisted = true;
+        }
+
+        $whiteListLiteralRules = $activeRules->where('rule_type', 1);
+        $whiteListRegexRules = $activeRules->where('rule_type', 2);
+
+        if ($blacklisted) {
+            if (static::checkLiteral($email, $whiteListLiteralRules, true)) {
+                return;
+            } else {
+                $this->raiseBlacklistException();
+            }
+        }
+
+        if (count($whiteListLiteralRules) && count($whiteListRegexRules) === 0) {
+            return;
+        }
+
+        if (static::checkLiteral($email, $whiteListLiteralRules, true) || static::checkRegex($email, $whiteListRegexRules, true)) {
+            return;
+        }
+
+        $this->raiseWhitelistException();
     }
 
-    $whiteListLiteralRules = $activeRules->where('rule_type', 1);
-    $whiteListRegexRules = $activeRules->where('rule_type', 2);
-
-    if ($blacklisted) {
-      if (static::checkLiteral($email, $whiteListLiteralRules)) {
-        return;
-      } else {
-        $this->raiseBlacklistException();
-      }
+    private function raiseBlacklistException()
+    {
+        $message = $this->settings->get('nyu8-email-filter.custom_failure_message', $this->translator->trans('nyu8-email-filter.forum.blacklist_exception'));
+        throw new ValidationException([$message]);
     }
 
-    if (count($whiteListLiteralRules) && count($whiteListRegexRules) === 0) {
-      return;
+    private function raiseWhitelistException()
+    {
+        $message = $this->settings->get('nyu8-email-filter.custom_failure_message', $this->translator->trans('nyu8-email-filter.forum.whitelist_exception'));
+        throw new ValidationException([$message]);
     }
 
-    if (static::checkLiteral($email, $whiteListLiteralRules) || static::checkRegex($email, $whiteListRegexRules)) {
-      return;
+    /**
+     * @param Rule[] $rules
+     */
+    private static function checkRegex(string $email, array $rules, bool $caseSensitive = false): bool
+    {
+        foreach ($rules as $rule) {
+            if (preg_match('/' . $rule->value . '/' . ($caseSensitive ? '' : 'i'), $email, $_match)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
-    $this->raiseWhitelistException();
-  }
+    /**
+     *
+     * @param Rule[] $rules
+     */
+    private static function checkLiteral(string $email, array $rules, bool $caseSensitive = false): bool
+    {
+        foreach ($rules as $rule) {
+            if ($caseSensitive && $email == $rule->value) {
+                return true;
+            } else if (!$caseSensitive && strtolower($email) == strtolower($rule->value)) {
+                return true;
+            }
+        }
 
-  private function raiseBlacklistException()
-  {
-    $message = $this->settings->get('nyu8-email-filter.custom_failure_message', $this->translator->trans('nyu8-email-filter.forum.blacklist_exception'));
-    throw new ValidationException([$message]);
-  }
-
-  private function raiseWhitelistException()
-  {
-    $message = $this->settings->get('nyu8-email-filter.custom_failure_message', $this->translator->trans('nyu8-email-filter.forum.whitelist_exception'));
-    throw new ValidationException([$message]);
-  }
-
-  /**
-   * @param Rule[] $rules
-   */
-  private static function checkRegex(string $email, $rules)
-  {
-    foreach ($rules as $rule) {
-      if (preg_match('/' . $rule->value . '/', $email, $_match)) {
-        return true;
-      }
+        return false;
     }
-
-    return false;
-  }
-
-  /**
-   *
-   * @param Rule[] $rules
-   */
-  private static function checkLiteral(string $email, $rules)
-  {
-    foreach ($rules as $rule) {
-      if ($email == $rule->value) {
-        return true;
-      }
-    }
-
-    return false;
-  }
 }
